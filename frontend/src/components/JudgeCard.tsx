1c72b0d2bc943c71f7847beb6c99c4ee1a4f33b6
Shane Yokota <shaneyok@usc.edu>
2025-08-10 13:09:13 -0700

    feat: img gen flow comp

--- frontend/src/components/JudgeCard.tsx
+++ frontend/src/components/JudgeCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 export default function JudgeCard({
     title,
     handleJudged,
@@ -9,18 +11,29 @@ export default function JudgeCard({
     imageData: string,
     isLeft: boolean
 }) {
+  const [isSelected, setIsSelected] = useState(false)
+  const [finishedSelection, setFinishedSelection] = useState(false)
+
+  const handleClick = () => {
+    setFinishedSelection(true)
+    setIsSelected(true)
+    setTimeout(() => {}, 300)
+    handleJudged(isLeft ? 1 : 2)
+  }
+
   return (
-    <div className='!w-full mx-2 flex flex-col items-center'>
-        <div className='w-full h-auto aspect-[9/13] rounded-2xl bg-gray-400 '>
-          <img src={imageData} alt={title} className='w-full h-full rounded-2xl object-contain'/>
-        </div>
-        <p className='text-xl text-center font-semibold my-4 h-14'>{title}</p>
-         <button
-          onClick={() => handleJudged(isLeft ? 1 : 2)}
-          className="text-white bg-[#5433EB] rounded-full py-2 px-4"
-        >
-          {isLeft ? 'Left' : 'Right'}
-        </button>
+    // rounded-lg bg-green-300/25
+    <div className={`!w-full p-2 flex flex-col items-center ${finishedSelection && isSelected ? 'bg-green-300/25' : ''} rounded-lg transition-all duration-300`}>
+      <div className='w-full h-auto aspect-[9/13] rounded-2xl bg-gray-400 '>
+        <img src={imageData} alt={title} className='w-full h-full rounded-2xl object-contain'/>
+      </div>
+      <p className='text-xl text-center font-semibold my-4 h-14'>{title}</p>
+        <button
+        onClick={handleClick}
+        className="text-white bg-[#5433EB] rounded-full py-2 px-4"
+      >
+        {isLeft ? 'Left' : 'Right'}
+      </button>
     </div>
   )
 }
