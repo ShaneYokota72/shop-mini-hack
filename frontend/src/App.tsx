d143ecbb52e227430113ec9b21abfc6b56be67cc
sun-jay <sunny.jyrm@gmail.com>
2025-08-10 14:05:05 -0700

    better flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import {Whiteboard} from './components/Whiteboard/Whiteboard'
 import {Submission} from './components/Submission'
 import {Judging} from './components/Judging'
 import {Results} from './components/Results'
+import {Loading} from './components/Loading'
 import Winner from './components/Winner'
 
 export function App() {
@@ -13,8 +14,9 @@ export function App() {
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/whiteboard" element={<Whiteboard />} />
-      <Route path="/submission" element={<Submission />} />
       <Route path="/judging" element={<Judging />} />
+      <Route path="/loading" element={<Loading />} />
+      <Route path="/submission" element={<Submission />} />
       <Route path="/results" element={<Results />} />
       <Route path="/winners-1" element={<Winner />} />
       <Route path="/winners-2" element={<Winner />} />
